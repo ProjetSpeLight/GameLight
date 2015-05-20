de147112dc9b4dfca41443c979d828338280b34b
denisPoifol <denis.poifol@gmail.com>
2015-05-20 15:33:12 +0200

    creation ecran d'acceuil et base de jeu plateforme

--- GAME/GAME/mainmenu.js
+++ GAME/GAME/mainmenu.js
@@ -3,18 +3,21 @@
 };
 
 GameStates.MainMenu.prototype = {
-    create: function () {
-        // create main menu text and images -
-        // create a "Start Game" mechanism - variety of ways to do this...
+        create: function () {
+                // create main menu text and images -
+                // create a "Start Game" mechanism - variety of ways to do this...
 
-        this.loadingText = this.add.text(this.game.width / 2, this.game.height / 2, "Press Enter to start", { font: "20px monospace", fill: "#fff" });
-        this.loadingText.anchor.setTo(0.5, 0.5);
+                //below code creates a simple tween animation. You will want to delete this when adding your code
+                var logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo');
+                logo.anchor.setTo(0.5, 0.5);
+                logo.scale.setTo(0.2, 0.2);
+                this.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
 
-        this.enterKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
+                this.enterKey = this.game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
 
-        this.enterKey.onDown.add(this.playGame, this);
-    },
-    playGame: function () {
-        this.state.start('Game');
-    }
+                this.enterKey.onDown.add(this.playGame, this);
+        },
+        playGame: function () {
+                this.state.start('Game');
+        }
 };
