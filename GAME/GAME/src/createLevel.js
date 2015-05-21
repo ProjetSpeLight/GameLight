1302c0f0e6bf2c6d03c44d70efe5ac0390f2d6f3
Cédric Garnier <cedric.garnier@ensimag.grenoble-inp.fr>
2015-05-21 17:47:41 +0200

    ajout etoile correction

--- GAME/GAME/src/createLevel.js
+++ GAME/GAME/src/createLevel.js
@@ -111,7 +111,7 @@ function createStart(element,game) {
     //  Here we'll create 12 of them evenly spaced apart
     for (var i = 0; i < 12; i++) {
         //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
+        var star = stars.create(i * 70, 0, 'coin');
 
         //  Let gravity do its thing
         star.body.gravity.y = 300;
