e4ac9872b2b8e7feaccbcd02c5bbc72315ced884
DavidTordjman <david.tordjman@outlook.fr>
2015-05-31 18:13:52 +0200

    Définition de la structure des actions dans le JSON
    L'ajout des actions dans un JSON est présentée en commentaires au début
    du fichier

--- GAME/GAME/requirejs/app/objects/action.js
+++ GAME/GAME/requirejs/app/objects/action.js
@@ -1,5 +1,29 @@
+/**
+  * This module implements the differents actions that can occurs after an event on a switch or a button
+  *
+  * Structure of the JSON file related to the declaration of actions
+  *
+  * {
+  *     ... // Definition of the switch or button
+  *     action: {
+  *         groupId : {Number} // Id of the group of the target, i.e. which type of objects it is (mirror, filter, platform...)
+  *         id : {Number} // Id of the target within its group
+  *         actionName: {String} // Name of the action to define
+  *         args: {Array} // Array of the arguments necessary to the action
+  *             {
+  *                 arg1:
+  *                 arg2:
+  *                 ...
+  *             }
+  * }
+  *
+  **/
 define([], function () {
 
+    /// @function createAction
+    /// Creates and returns an array composed of the different elements of an action : the target, the action function and its argument(s) from the JSON file
+    /// @return {Object} an object containing the necessary data to perform the action when the signal is activated
+    /// @param {Object} Object created from the JSON parse
     function createAction(data) {
         return;
     }
