3b83a209015b3bcb9a2a45bf029f620d94f4e2ae
Cédric Garnier <cedric.garnier@ensimag.grenoble-inp.fr>
2015-06-05 17:27:18 +0200

    Ajout Affichage score dans chooseLevel grâce aux cookies
    Attention , pas de verification si l’ancien score est meilleur pour
    l’instant ou pas

--- GAME/GAME/requirejs/app/states/chooseLevelState.js
+++ GAME/GAME/requirejs/app/states/chooseLevelState.js
@@ -10,9 +10,21 @@ define([
             // We generate a text and a button (sprite) per level
             var x = 10;
             var y = 10;
+            // We read the cookies of the game
+            var cook = document.cookie ;
             for (var i = 1 ; i <= this.game.nbLevel ; i++) {
                 var emptyButton = this.game.add.button(x, y, 'buttonEmpty', this.down, self);
                 var text = this.game.add.text(x + 50, y + 15, "Level " + i, { font: "28px Arial", fill: "#ffffff", align: "center" });
+                var textResult= this.game.add.text(x + 200, y+15, " Score Done :\nNot Finish" ,  { font: "18px Arial", fill: "#ffffff", align: "center" });
+                
+                // We search the label 'Level i'
+                var sub = cook.indexOf("Level"+i,1);
+                //if this label exists,
+                //we change the text to print the score
+                if (sub>0){
+                textResult.text=" Score Done : "+cook.substring(sub+7,sub+9);    
+                }
+                
                 emptyButton.numLevel = i;
                 emptyButton.refGame = this;
                 y += emptyButton.height + 20;
@@ -33,7 +45,10 @@ define([
 
         returnMenu: function () {
             this.game.state.start('MainMenu');
-        }
+        },
+        
+        
+
 
 
 
