6a76382362a01502c4717b181a3233aa7b13f0f6
DavidTordjman <david.tordjman@outlook.fr>
2015-06-06 15:48:48 +0200

    Amélioration affichage

--- GAME/GAME/requirejs/app/states/chooseLevelState.js
+++ GAME/GAME/requirejs/app/states/chooseLevelState.js
@@ -14,8 +14,15 @@ define([
             var cook = document.cookie ;
             for (var i = 1 ; i <= this.game.nbLevel ; i++) {
                 var emptyButton = this.game.add.button(x, y, 'buttonEmpty', this.down, self);
-                var text = this.game.add.text(x + 50, y + 15, "Level " + i, { font: "28px Arial", fill: "#ffffff", align: "center" });
-                var textResult= this.game.add.text(x + 200, y+15, " Score Done :\nNot Finish" ,  { font: "18px Arial", fill: "#ffffff", align: "center" });
+                emptyButton.scale.setTo(0.8, 0.8);
+
+                var posLabelButtonX = x + 30;
+                var posLabelButtonY = y + 9;
+                var posLabelScoreX = x + 160;
+                var posLabelScoreY = y+7;
+
+                var text = this.game.add.text(posLabelButtonX, posLabelButtonY, "Level " + i, { font: "28px Arial", fill: "#ffffff", align: "center" });
+                var textResult = this.game.add.text(posLabelScoreX, posLabelScoreY, " Score Done :\nNot Finish", { font: "18px Arial", fill: "#ffffff", align: "center" });
                 
                 // We search the label 'Level i'
                 var sub = cook.lastIndexOf("Level"+i,1);
@@ -30,11 +37,12 @@ define([
                 y += emptyButton.height + 20;
                 if (y > this.game.world.height - emptyButton.height) {
                     y = 10;
-                    x += emptyButton.width + 20;
+                    x += posLabelScoreX + textResult.width + 40;
                 }
             }
 
             var buttonMenu = this.game.add.button(this.game.world.width - 10, this.game.world.height - 10, 'RetMenu', this.returnMenu, this);
+            buttonMenu.scale.setTo(0.8, 0.8);
             buttonMenu.anchor.setTo(1, 1);
         },
 
